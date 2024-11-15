393e2e9a36fae2dfb9d5689fce135c6fd1aff1c4
Will G. <greenlee04@gmail.com>
2024-11-15 15:43:13 -0500

    enable nvidia power management

--- modules/machines/nvidia.nix
+++ modules/machines/nvidia.nix
@@ -11,6 +11,8 @@
     graphics.enable = true; # enable hardeware graphics acceleration
     nvidia = {
       modesetting.enable = true; # necessary for wayland
+      powermanagement.enable = true; # save entire vram to /tmp on suspend
+      powermanagement.finegrained = true; # turn off GPU when not in use
       open = true; # open kernel module
       nvidiaSettings = true; # settings menu accessible in OS
     };
