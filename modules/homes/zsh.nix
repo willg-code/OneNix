07274e96967919545026ba988fd9dc8296f3f2be
Will G. <greenlee04@gmail.com>
2024-11-15 16:35:37 -0500

    zsh updates

--- modules/homes/zsh.nix
+++ modules/homes/zsh.nix
@@ -3,13 +3,16 @@
   programs.zsh = {
     # shell with plugins
     enable = true;
-    autosuggestion.enable = true;
+    autocd = true; # automatically CD a directory if typed directly
+    autosuggestion = {
+      enable = true; # automatically suggest completions
+      strategy = [
+        "history" # based on command history
+        "completion" #based on tab completion
+      ];
+    };
     syntaxHighlighting.enable = true;
     history.path = "$HOME/.cache/zsh_history"; # so it's cleared with the cache
-    oh-my-zsh = {
-      # zsh plugin manager
-      enable = true;
-      plugins = [ "git" "z" ];
-    };
+    dotDir = "$HOME/.config/zsh";
   };
 }
