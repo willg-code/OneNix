c76275456ca0d296b44aea10027c36b7c1e7e46d
Will G. <greenlee04@gmail.com>
2024-11-15 18:17:26 -0500

    remove plugins

--- modules/homes/zsh.nix
+++ modules/homes/zsh.nix
@@ -3,15 +3,6 @@
   programs.zsh = {
     # shell with plugins
     enable = true;
-    antidote =
-      {
-        enable = true;
-        plugins = [
-          "zsh-users/zsh-autosuggestions"
-          "chrissicool/zsh-256color"
-          "davidde/git"
-        ];
-      };
     autocd = true; # automatically CD a directory if typed directly
   };
 }
