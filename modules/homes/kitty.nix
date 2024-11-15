f9a9d99b7e427a56a1f3ba133c7db7d2b434bf29
Will G. <greenlee04@gmail.com>
2024-11-15 18:15:39 -0500

    no scrollback line limit

--- modules/homes/kitty.nix
+++ modules/homes/kitty.nix
@@ -5,10 +5,9 @@
     enable = true; # hyprland default terminal emulator
     settings = {
       shell = "zsh";
-      scrollback_lines = 10000;
       enable_audio_bell = false;
       update_check_interval = 0;
-      background_opacity = "0.75";
+      background_opacity = "0.8";
     };
   };
 
