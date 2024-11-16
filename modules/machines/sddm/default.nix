80753bccf59a7aea18cd4707dab6885cf9af87fa
Will G. <greenlee04@gmail.com>
2024-11-16 18:15:21 -0500

    comment updates

--- modules/machines/sddm/default.nix
+++ modules/machines/sddm/default.nix
@@ -17,6 +17,6 @@
     enable = true; # login screen compatible with hyprland
     wayland.enable = true; # native wayland support
     theme = "catppuccin-mocha"; # enable the previously declared theme
-    package = pkgs.kdePackages.sddm; # working package to fix module
+    package = pkgs.kdePackages.sddm; # more up-to-date package to fix theme
   };
 }
