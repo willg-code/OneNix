0c8e99085672df696917af9a006cc003eeb9a0f2
Will G. <greenlee04@gmail.com>
2024-11-15 21:44:18 -0500

    change sddm theme

--- modules/machines/sddm.nix
+++ modules/machines/sddm.nix
@@ -3,12 +3,12 @@
 
 {
   environment.systemPackages = [
-    pkgs.sddm-astronaut # theme package
+    pkgs.catppuccin-sddm # theme package
   ];
 
   services.displayManager.sddm = {
     enable = true; # login screen compatible with hyprland
     wayland.enable = true; # native wayland support
-    theme = "sddm-astronaut-theme"; # enable the previously declared theme
+    theme = "catppuccin-mocha"; # enable the previously declared theme
   };
 }
