cd8b8b07017a1686319d293ef7f4cda2ee97c2cd
Will G. <greenlee04@gmail.com>
2024-11-18 18:41:00 -0500

    add clean date and rm gc config

--- modules/homes/nh.nix
+++ modules/homes/nh.nix
@@ -6,9 +6,8 @@
     enable = true; # cli helper
     clean = {
       enable = true; # garbage collector
+      dates = "daily"; # clean daily
       extraArgs = "--keep-since 3d"; # keep generations newer than 3 days
     };
   };
-
-  nix.gc.automatic = false;
 }
