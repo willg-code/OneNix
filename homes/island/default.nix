338690771b3e0cd336345a2364e45942517337ec
Will G. <greenlee04@gmail.com>
2024-11-18 18:25:36 -0500

    add flake path

--- homes/island/default.nix
+++ homes/island/default.nix
@@ -44,5 +44,7 @@ user:
   programs.git.userEmail = user.email;
   programs.git.userName = user.name;
 
+  programs.nh.flake = /home/${user.username}/OneNix;
+
   home.stateVersion = "24.05"; # Required for backwards compat during updates
 }
