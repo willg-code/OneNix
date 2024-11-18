64a1436bd63b4a9aeab70e6235985eb55e79e490
Will G. <greenlee04@gmail.com>
2024-11-17 20:35:51 -0500

    add comments

--- modules/homes/kitty.nix
+++ modules/homes/kitty.nix
@@ -4,10 +4,10 @@
   programs.kitty = {
     enable = true; # hyprland default terminal emulator
     settings = {
-      shell = "zsh";
-      enable_audio_bell = false;
-      update_check_interval = 0;
-      background_opacity = "0.8";
+      shell = "zsh"; # shell command to execute at start time
+      enable_audio_bell = false; # no sound
+      update_check_interval = 0; # don't check for updates
+      background_opacity = "0.8"; # slightly transparent background
     };
   };
 
