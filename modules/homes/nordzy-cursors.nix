80d24501303a78e0289fa6dd4f4e21f3b4da6d9c
Will G. <greenlee04@gmail.com>
2024-11-17 21:16:20 -0500

    fix cursor

--- modules/homes/nordzy-cursors.nix
+++ modules/homes/nordzy-cursors.nix
@@ -9,4 +9,13 @@
   wayland.windowManager.hyprland.settings.exec-once = [
     "hyprctl setcursor Nordzy-cursors 24"
   ];
+
+  # XCursor/GTK integration (for apps that do not support hyprcursor)
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.nordzy-cursor-theme;
+    name = "Nordzy-cursors";
+    size = 24;
+  };
 }
