820e0d2c35b5341849f07d80b2a02d192f0fde90
Will G. <greenlee04@gmail.com>
2024-11-17 23:45:00 -0500

    fix hostname

--- machines/andromeda/default.nix
+++ machines/andromeda/default.nix
@@ -1,6 +1,6 @@
 modules:
 
-{ inputs, name, ... }:
+{ inputs, hostname, ... }:
 
 {
   imports = [
@@ -15,7 +15,7 @@ modules:
   ];
 
   time.timeZone = "America/Louisville"; # based on location of the machine
-  networking.hostName = name; # network host name
+  networking.hostName = hostname; # network host name
   networking.networkmanager.enable = true; # automatically connects to network when possible
   system.stateVersion = "24.05";
 }
