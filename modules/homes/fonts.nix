75acaff94ed3f5f4bd74fa07054c407f4619954c
Will G. <greenlee04@gmail.com>
2024-11-19 13:46:46 -0500

    remove overpass

--- modules/homes/fonts.nix
+++ modules/homes/fonts.nix
@@ -4,15 +4,10 @@
 {
   fonts.fontconfig = {
     enable = true; # allow font package discovery from home.packages
-    defaultFonts = {
-      monospace = [ "Overpass Mono" ];
-      sansSerif = [ "Overpass" ];
-    };
   };
 
   home.packages = [
     pkgs.nerdfonts # code fonts
-    pkgs.overpass # pretty sans-serif/mono font
     pkgs.noto-fonts # everything else
   ];
 }
