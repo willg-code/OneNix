0208cf915549b6f1a4f8452ad0fc7ff2d22ca4ae
Will G. <greenlee04@gmail.com>
2024-11-20 15:34:30 -0500

    re-enable auto reload

--- modules/homes/hyprland/default.nix
+++ modules/homes/hyprland/default.nix
@@ -71,7 +71,6 @@ in
         misc = {
           disable_hyprland_logo = "true"; # disable default hyprland backgrounds
           disable_splash_rendering = "true"; # disable splash text
-          disable_autoreload = "true"; # disable script reload, not needed bc nixos
           focus_on_activate = "true"; # apps can request focus
           vrr = "2"; # use variable refresh rate (if available) when fullscreen
         };
