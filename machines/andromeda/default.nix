dd241f7045d4277364e78600d1ca0bd95d212d37
Will G. <greenlee04@gmail.com>
2024-11-19 19:42:54 -0500

    enable boot

--- machines/andromeda/default.nix
+++ machines/andromeda/default.nix
@@ -6,6 +6,10 @@
     ./hardware-configuration.nix
   ];
 
+  # Modules
+  modules.machines.systemd-boot.enable = true;
+
+  # Other
   time.timeZone = "America/Louisville"; # based on location of the machine
   networking.hostName = hostname; # network host name
   system.stateVersion = "24.05";
