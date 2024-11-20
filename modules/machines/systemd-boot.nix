cce7c0fe4e29c6f2cc8b171ec595c8439b54d829
Will G. <greenlee04@gmail.com>
2024-11-19 19:40:26 -0500

    module refactor

--- modules/machines/systemd-boot.nix
+++ modules/machines/systemd-boot.nix
@@ -2,15 +2,25 @@
 # DOCS: https://nixos.wiki/wiki/Grub
 { ... }:
 
+let
+  name = "systemd-boot";
+  cfg = config.modules.machines.name;
+in
 {
-  boot = {
-    loader.systemd-boot = {
-      enable = true; # enable bootloader
-      editor = false; # no command line boot entry; security risk
-      memtest86.enable = true; # mem test available in bootloader
-      configurationLimit = 10; # max number of configuration boot entries
+  options.modules.machines.${name} = {
+    enable = lib.mkEnableOption name;
+  };
+
+  config = lib.mkIf cfg.enable {
+    boot = {
+      loader.systemd-boot = {
+        enable = true; # enable bootloader
+        editor = false; # no command line boot entry; security risk
+        memtest86.enable = true; # mem test available in bootloader
+        configurationLimit = 10; # max number of configuration boot entries
+      };
+      loader.efi.canTouchEfiVariables = true; # allows installer to change efi variables (to enable boot option)
+      tmp.cleanOnBoot = true; # helps cut down on disk usage, tmp is safe to clean at boot
     };
-    loader.efi.canTouchEfiVariables = true; # allows installer to change efi variables (to enable boot option)
-    tmp.cleanOnBoot = true; # helps cut down on disk usage, tmp is safe to clean at boot
   };
 }
