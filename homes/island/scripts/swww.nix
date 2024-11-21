a1220b667490bc0a2d9dca3841e22b603df172e5
Will G. <greenlee04@gmail.com>
2024-11-21 01:30:54 -0500

    escape vars

--- homes/island/scripts/swww.nix
+++ homes/island/scripts/swww.nix
@@ -8,7 +8,7 @@ pkgs.writeShellScript "swww.sh" ''
   fi
 
   # Set the wallpaper to start
-  swww img "${1}" --transition-type none
+  swww img "''${1}" --transition-type none
 
   # This controls (in seconds) when to switch to the next image
   INTERVAL=300
@@ -20,16 +20,16 @@ pkgs.writeShellScript "swww.sh" ''
       # Pick an image based on the time of day
       if [ "$current_hour" -ge 6 ] && [ "$current_hour" -lt 9 ]; then
           # Morning: 6am - 8:59am
-          img="${1}"
+          img="''${1}"
       elif [ "$current_hour" -ge 9 ] && [ "$current_hour" -lt 17 ]; then
           # Day: 10am - 4:59pm
-          img="${2}"
+          img="''${2}"
       elif [ "$current_hour" -ge 17 ] && [ "$current_hour" -lt 20 ]; then
           # Evening 5pm - 7:59pm
-          img="${3}"
+          img="''${3}"
       else
           # Night: 8pm - 5:59am
-          img="${4}"
+          img="''${4}"
       fi
 
       # Set the wallpaper using swww
