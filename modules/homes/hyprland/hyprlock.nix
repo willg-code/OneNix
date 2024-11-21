ffe5bc3269065ec3da250b60f02e3e6001d8dbbb
Will G. <greenlee04@gmail.com>
2024-11-21 00:59:38 -0500

    fix lock screen

--- modules/homes/hyprland/hyprlock.nix
+++ modules/homes/hyprland/hyprlock.nix
@@ -14,7 +14,7 @@ in
         };
       background = [
         {
-          monitor = ""; # active monitor
+          monitor = ""; # all monitors
           path = if bgPath != null then (toString bgPath) else ""; # use screenshot of desktop
           color = "rgba(${colors.neutralDark}FF)"; # fallback color for no background
           blur_passes = "3"; # smooth blur
@@ -23,7 +23,7 @@ in
       input-field = [
         # Input Box
         {
-          monitor = ""; # all monitors
+          monitor = "";
           size = "300, 60";
           outline_thickness = "2";
           dots_size = "0.1"; # Scale of input-field height, 0.2 - 0.8
@@ -32,31 +32,39 @@ in
           inner_color = "rgba(${colors.neutralDark}A5)";
           font_color = "rgb(${colors.neutralLight})";
           position = "0, -200"; # offset from center of screen;
+          halign = "center";
+          valign = "center";
         }
       ];
       label = [
         # Time
         {
           monitor = "";
-          text = ''cmd[update:1000] echo "$(date +"%A, %B %d")"'';
-          color = "rgba(242, 243, 244, 0.8)";
+          text = ''cmd[update:1000] echo "$(date +"%A, %B %d")"''; # get the date
+          color = "rgba(${colors.neutralLight}D5)";
           font_size = "22";
           position = "0, 300"; # offset from center of screen
+          halign = "center";
+          valign = "center";
         }
         {
           monitor = "";
-          text = "$TIME";
-          color = "rgba(242, 243, 244, 0.8)";
+          text = "$TIME"; # time provided by hyprlock
+          color = "rgba(${colors.neutralLight}D5)";
           font_size = "95";
           position = "0, 200"; # offset from center of screen
+          halign = "center";
+          valign = "center";
         }
         # User Greeting
         {
           monitor = "";
-          text = "Hello $DESC";
-          color = "rgba(242, 243, 244, 0.8)";
+          text = "Hello $DESC"; # description provided by user
+          color = "rgba(${colors.neutralLight}D5)";
           font_size = "22";
           position = "0, -150"; # offset from center of screen
+          halign = "center";
+          valign = "center";
         }
       ];
     };
