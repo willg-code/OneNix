a2e97043c3bff07d70cab95370202ed0bc44d6a4
Will G. <greenlee04@gmail.com>
2024-11-20 21:05:21 -0500

    reorder

--- modules/homes/hyprland/general.nix
+++ modules/homes/hyprland/general.nix
@@ -5,9 +5,9 @@ let
 in
 {
   wayland.windowManager.hyprland.settings.general = {
+    border_size = "2"; # size (in pixels) of winsow border
     gaps_in = "5"; # size (in pixels) of gap between the windows
     gaps_out = "5"; # size (in pixels) of gap on the edge of the screen
-    border_size = "2"; # size (in pixels) of winsow border
     "col.active_border" = "rgba(${colors.primaryHighlight}FF)";
     "col.inactive_border" = "rgba(${colors.primaryLowlight}A5)";
     snap.enabled = true; # snap floating windows to edges
