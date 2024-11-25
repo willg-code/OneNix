10a9fec68bc3b0d7cc30552884ea2ffff4fbd1dd
Will G. <greenlee04@gmail.com>
2024-11-24 22:16:44 -0500

    auto parse default secrets file

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -1,9 +1,16 @@
-{ inputs, ... }:
+{ inputs, secrets, ... }:
 
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
 
-  sops = { };
+  sops = {
+    defaultSopsFile = secrets.secrets;
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ]; # import ssh key as age key
+      keyFile = "/var/lib/sops-nix/key.txt"; # output age key here
+      generateKey = true; # generate it if not found
+    };
+  };
 }
