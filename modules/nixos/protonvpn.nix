7c74d5df1c0196a6dcf1c8e345d21127721799c0
willg <greenlee04@gmail.com>
2024-11-25 17:05:22 -0500

    enable device

--- modules/nixos/protonvpn.nix
+++ modules/nixos/protonvpn.nix
@@ -13,19 +13,22 @@ in
   config = lib.mkIf cfg.enable {
     sops.secrets."proton_key" = { };
 
-    # Config
-    systemd.network.netdevs.protonvpn.wireguardConfig = {
-      PrivateKeyFile = config.sops.secrets."proton_key".path;
-      Address = "10.2.0.2/32";
-      DNS = "10.2.0.1";
-    };
 
-    #Peers
-    systemd.network.netdevs.protonvpn.wireguardPeers = {
-      # US-IL#159
-      PublicKey = "xuqP9uEGryELhamLSK9IDRNhljo3lA1zL9/gS7yj2WQ=";
-      AllowedIPs = "0.0.0.0/0";
-      Endpoint = "87.249.134.139:51820";
+    systemd.network.netdevs.protonvpn = {
+      enable = true; # VPN Device
+      # Config
+      wireguardConfig = {
+        PrivateKeyFile = config.sops.secrets."proton_key".path;
+        Address = "10.2.0.2/32";
+        DNS = "10.2.0.1";
+      };
+      #Peers
+      wireguardPeers = {
+        # US-IL#159
+        PublicKey = "xuqP9uEGryELhamLSK9IDRNhljo3lA1zL9/gS7yj2WQ=";
+        AllowedIPs = "0.0.0.0/0";
+        Endpoint = "87.249.134.139:51820";
+      };
     };
   };
 }
