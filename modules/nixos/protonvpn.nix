90aa80232cc1d89e5a2e1c914686b9d0feb9eac0
willg <greenlee04@gmail.com>
2024-11-25 17:00:01 -0500

    enable proton config

--- modules/nixos/protonvpn.nix
+++ modules/nixos/protonvpn.nix
@@ -1,3 +1,31 @@
-{ ... }:
+# Proton VPN configuration
+{ config, lib, ... }:
 
-{ }
+let
+  moduleName = "protonvpn";
+  cfg = config.modules.nixos.${moduleName};
+in
+{
+  options.modules.nixos.${moduleName} = {
+    enable = lib.mkEnableOption moduleName;
+  };
+
+  config = lib.mkIf cfg.enable {
+    sops.secrets."proton_key" = { };
+
+    # Config
+    systemd.network.netdevs.protonvpn.wireguardConfig = {
+      PrivateKeyFile = config.sops.secrets."proton_key".path;
+      Address = "10.2.0.2/32";
+      DNS = "10.2.0.1";
+    };
+
+    #Peers
+    systemd.network.netdevs.protonvpn.wireguardPeers = {
+      # US-IL#159
+      PublicKey = "xuqP9uEGryELhamLSK9IDRNhljo3lA1zL9/gS7yj2WQ=";
+      AllowedIPs = "0.0.0.0/0";
+      Endpoint = "87.249.134.139:51820";
+    };
+  };
+}
