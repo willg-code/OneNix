6775b05a8eb0041b6a4736238a34826124132cb9
willg <greenlee04@gmail.com>
2024-11-27 19:40:46 -0500

    remove comment

--- modules/home-manager/hyprland/windowrules.nix
+++ modules/home-manager/hyprland/windowrules.nix
@@ -2,7 +2,6 @@
 
 {
   wayland.windowManager.hyprland.settings = {
-    # These make the behavior of apps more predictable
     windowrulev2 = [
       "suppressevent maximize, class:.*" # no auto maximization
       "suppressevent fullscreen, class:.*" # no auto fullscreen
