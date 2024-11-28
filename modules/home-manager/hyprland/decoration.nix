4de6bbd9baec90e397a426277ade9291293f65d7
willg <greenlee04@gmail.com>
2024-11-27 19:16:16 -0500

    revert

--- modules/home-manager/hyprland/decoration.nix
+++ modules/home-manager/hyprland/decoration.nix
@@ -7,9 +7,6 @@
     dim_strength = "0.1"; # how much to darken inactives
     dim_special = "0.1"; # how much to darken screen during special workspace
     blur.passes = "3"; # smoother blur
-    blur.popups = "false";
-    blur.special = "false";
-    blur.input_methods = "false";
     shadow.range = "5"; # 5px shadow
   };
 }
