b8fb59f7f97a802b013942d39d2584d5671dbaab
William <greenlee04@gmail.com>
2024-11-30 13:13:51 -0500

    add disko

---
+++ machines/andromeda/disko.nix
@@ -0,0 +1,52 @@
+disko.devices = {
+  nodev = {
+    "/" ={
+      fstype = "tmpfs";
+      mountOptions = [ "defaults" "size=25%" "mode=755" ];
+    };
+  };
+  disk = {
+    sdd = {
+      device = "/dev/disk/by-id/wwn-0x5f8db4c141803e8c";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "256M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          persistent = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "bcachefs";
+              mountpoint = "/persistent";
+            };
+          };
+        };
+      };
+    };
+    hdd = {
+      device = "/dev/disk/by-id/wwn-0x50014ee20e131e19";
+      content = {
+        type = "gpt";
+        partitions = {
+          persistent = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "bcachefs";
+              mountpoint = "/persistent/home";
+            };
+          };
+        };
+      };
+    }
+  };
+}
--- machines/andromeda/hardware.nix
+++ machines/andromeda/hardware.nix
@@ -17,22 +17,6 @@
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/024c4fda-fa87-4d19-9168-b674658cc7b7";
-    fsType = "btrfs";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/2326-A980";
-    fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/e6121161-e6a4-46fe-b9b5-e4e7c437e075";
-    fsType = "btrfs";
-  };
-
   swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
