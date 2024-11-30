901211add02364d27b8858ede3a79d5d75d86eec
William <greenlee04@gmail.com>
2024-11-30 13:20:28 -0500

    make into module

--- machines/andromeda/disko.nix
+++ machines/andromeda/disko.nix
@@ -1,48 +1,50 @@
-disko.devices = {
-  nodev = {
-    "/" ={
-      fstype = "tmpfs";
-      mountOptions = [ "defaults" "size=25%" "mode=755" ];
+{
+  disko.devices = {
+    nodev = {
+      "/" ={
+        fstype = "tmpfs";
+        mountOptions = [ "defaults" "size=25%" "mode=755" ];
+      };
     };
-  };
-  disk = {
-    sdd = {
-      device = "/dev/disk/by-id/wwn-0x5f8db4c141803e8c";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "256M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
+    disk = {
+      sdd = {
+        device = "/dev/disk/by-id/wwn-0x5f8db4c141803e8c";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "256M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
             };
-          };
-          persistent = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "bcachefs";
-              mountpoint = "/persistent";
+            persistent = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "bcachefs";
+                mountpoint = "/persistent";
+              };
             };
           };
         };
       };
-    };
-    hdd = {
-      device = "/dev/disk/by-id/wwn-0x50014ee20e131e19";
-      content = {
-        type = "gpt";
-        partitions = {
-          persistent = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "bcachefs";
-              mountpoint = "/persistent/home";
+      hdd = {
+        device = "/dev/disk/by-id/wwn-0x50014ee20e131e19";
+        content = {
+          type = "gpt";
+          partitions = {
+            persistent = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "bcachefs";
+                mountpoint = "/persistent/home";
+              };
             };
           };
         };
