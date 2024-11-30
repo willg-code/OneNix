7b614e2d556df18fab35a20bd31562500ad8848d
willg <greenlee04@gmail.com>
2024-11-30 02:10:46 -0500

    add bcachefs support

--- machines/installer/default.nix
+++ machines/installer/default.nix
@@ -5,5 +5,6 @@
 
   # Modules
   modules.nixos.optimize-store.enable = true;
+  boot.supportedFilesystems = ["bcachefs"]; # enable bcachefs install support
   nixpkgs.hostPlatform = "x86_64-linux";
 }
