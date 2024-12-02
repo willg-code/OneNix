bfcd422fcd7103151565e38195a5c30939dde979
William <greenlee04@gmail.com>
2024-12-02 14:03:58 -0500

    reorganize modules

--- homes/moon/default.nix
+++ homes/moon/default.nix
@@ -1,30 +1,34 @@
 {pkgs, ...}: {
-  # Modules
+  # Modules/CLI
   modules.home-manager.btop.enable = true;
   modules.home-manager.fastfetch.enable = true;
-  modules.home-manager.fuzzel.enable = true;
   modules.home-manager.git.enable = true;
+  modules.home-manager.nh.enable = true;
+  modules.home-manager.ssh.enable = true;
+  modules.home-manager.zsh.enable = true;
+
+  # Modules/GUI
   modules.home-manager.hyprland.enable = true;
-  modules.home-manager.hypridle.enable = true;
   modules.home-manager.hyprlock.enable = true;
+  modules.home-manager.fuzzel.enable = true;
+  modules.home-manager.hypridle.enable = true;
   modules.home-manager.hyprpicker.enable = true;
   modules.home-manager.hyprpolkitagent.enable = true;
   modules.home-manager.kitty.enable = true;
   modules.home-manager.mako.enable = true;
-  modules.home-manager.nh.enable = true;
-  modules.home-manager.notonerd-fonts.enable = true;
-  modules.home-manager.phinger-cursor.enable = true;
   modules.home-manager.screenshot.enable = true;
-  modules.home-manager.starship.enable = true;
   modules.home-manager.swww = {
     enable = true;
     switcher = pkgs.writeShellScript "moon.sh" ''swww img "${./assets/moon.jpg}" --transition-type none'';
   };
-  modules.home-manager.ssh.enable = true;
   modules.home-manager.vscode.enable = true;
   modules.home-manager.waybar.enable = true;
   modules.home-manager.zen-browser.enable = true;
-  modules.home-manager.zsh.enable = true;
+
+  # Modules/Style
+  modules.home-manager.notonerd-fonts.enable = true;
+  modules.home-manager.phinger-cursor.enable = true;
+  modules.home-manager.starship.enable = true;
 
   # Stylix
   stylix = {
