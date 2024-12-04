e72b78057aec07aa934dabbbef50e930b9a59414
William <greenlee04@gmail.com>
2024-12-04 15:44:07 -0500

    make sops key injectable

--- modules/nixos/core/sops.nix
+++ modules/nixos/core/sops.nix
@@ -7,13 +7,11 @@
   sops = {
     defaultSopsFile = inputs.self.outputs.secrets."secrets.yaml";
     age = {
-      sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"]; # import ssh key as age key
-      keyFile = "/var/lib/sops-nix/key.txt"; # output age key here
-      generateKey = true; # generate it if not found
+      keyFile = "/var/lib/sops-nix/key"; # output age key here
     };
   };
 
   environment.persistence."/persistent".files = [
-    "/var/lib/sops-nix/key.txt" # keep the key after it's generated
+    "/var/lib/sops-nix/key" # keep the key after it's generated
   ];
 }
