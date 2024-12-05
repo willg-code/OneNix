b6eb52f30bd375299180e9eed0286c5a605ed617
William <greenlee04@gmail.com>
2024-12-04 20:37:00 -0500

    temporarily change password

--- users/willg/default.nix
+++ users/willg/default.nix
@@ -13,7 +13,7 @@ home: {
       ["wheel"] # sudo user group
       ++ lib.optionals config.networking.networkmanager.enable ["networkmanager"] # non-sudo network manager permissions
       ++ lib.optionals config.virtualisation.docker.enable ["docker"]; # non-sudo docker permissions
-    password = "";
+    password = "temp";
     #hashedPasswordFile = config.sops.secrets."willg_password".path;
     openssh.authorizedKeys.keyFiles = [
       ./pubkeys/willg.pub # allow ssh from any other machine with this user account
