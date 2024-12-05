ed2b2c9404ecf924c8e8522132a641645c5bc6e5
willg <greenlee04@gmail.com>
2024-12-05 11:06:59 -0500

    add match block for identity

--- modules/home-manager/optional/cli/ssh.nix
+++ modules/home-manager/optional/cli/ssh.nix
@@ -18,6 +18,12 @@ in {
       enable = true; # ssh client
       addKeysToAgent = "1h"; # remember keys for 1 hour
       hashKnownHosts = true; # hash host IPs/names
+      matchBlocks = {
+        "default" = {
+          host = "*";
+          identityFile = config.sops.secrets."ssh_key".path;
+        };
+      }
     };
   };
 }
