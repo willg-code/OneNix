480c9239faebf02bfa86f45845e361873fc36354
willg <greenlee04@gmail.com>
2024-12-12 13:18:19 -0500

    cache dns

--- modules/nixos/optional/networkmanager.nix
+++ modules/nixos/optional/networkmanager.nix
@@ -14,5 +14,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     networking.networkmanager.enable = true; # networkmanager
+    #networking.nameservers = ["1.1.1.1"]; # Cloudflare
+    networking.nameservers = ["137.112.18.100"]; # Cache DNS
   };
 }
