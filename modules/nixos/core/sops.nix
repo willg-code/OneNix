cf2236ce3e9bf1fe91f4e04269f90f41ecb0b936
willg <greenlee04@gmail.com>
2024-12-15 16:18:28 -0500

    move reference to key

--- modules/nixos/core/sops.nix
+++ modules/nixos/core/sops.nix
@@ -6,10 +6,6 @@
 
   sops = {
     defaultSopsFile = inputs.self.outputs.secrets."secrets.yaml";
-    age.keyFile = "/var/lib/sops-nix/keys.txt";
+    age.keyFile = "/persistent/var/lib/sops-nix/keys.txt";
   };
-
-  environment.persistence."/persistent".files = [
-    "/var/lib/sops-nix/keys.txt"
-  ];
 }
