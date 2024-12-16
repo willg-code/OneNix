817c0f606eb4867bda8eef49741ef6888047ae5e
willg <greenlee04@gmail.com>
2024-12-16 12:25:22 -0500

    fix vulnerability

--- machines/installer/default.nix
+++ machines/installer/default.nix
@@ -1,8 +1,13 @@
-{modulesPath, ...}: {
+{
+  modulesPath,
+  lib,
+  ...
+}: {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
   # Modules
   nixpkgs.hostPlatform = "x86_64-linux";
+  services.openssh.settings.PermitRootLogin = lib.mkForce "no";
 }
--- modules/nixos/core/sshd.nix
+++ modules/nixos/core/sshd.nix
@@ -6,7 +6,7 @@
     enable = true; # ssh daemon (sshd)
     settings = {
       X11Forwarding = true; # allow x11 (or xwayland) windows to be forwarded
-      PermitRootLogin = lib.mkDefault "no"; # prohibit any root user logins because user accounts should be configured
+      PermitRootLogin = "no"; # prohibit any root user logins because user accounts should be configured
       PasswordAuthentication = false; # disable password auth because keys should be enabled on accounts
       KbdInteractiveAuthentication = false; # disable other non-key authentication
     };
