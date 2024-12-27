20f38f953978c0cd663d1255efeeba615b452994
willg <greenlee04@gmail.com>
2024-12-26 22:42:22 -0500

    setup zed

--- modules/home-manager/optional/gui/zen-browser.nix
+++ modules/home-manager/optional/gui/zen-browser.nix
@@ -15,7 +15,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      inputs.zen-browser.packages.${pkgs.system}
+      inputs.zen-browser.packages.${pkgs.system}.default
       pkgs.speechd # for speech synthesis
     ];
 
