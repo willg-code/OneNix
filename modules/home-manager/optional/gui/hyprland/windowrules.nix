f2d20ea63f8320cc7a8cf565f4245b09643e64d0
willg <greenlee04@gmail.com>
2024-12-27 01:58:22 -0500

    remove maximization block

--- modules/home-manager/optional/gui/hyprland/windowrules.nix
+++ modules/home-manager/optional/gui/hyprland/windowrules.nix
@@ -1,8 +1,6 @@
 {...}: {
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
-      "suppressevent maximize, class:.*" # no auto maximization
-      "suppressevent fullscreen, class:.*" # no auto fullscreen
       "noblur,xwayland:1" # don't blur xwayland windows, fixes popup issue
     ];
   };
