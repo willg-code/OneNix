8bd84a5407e215baa7e5ddecb1f58e2a453bb0df
willg <greenlee04@gmail.com>
2024-12-27 02:16:51 -0500

    add some env vars

--- modules/home-manager/core/uwsm.nix
+++ modules/home-manager/core/uwsm.nix
@@ -7,6 +7,8 @@
     enable = true;
     text = ''
       export ELECTRON_OZONE_PLATFORM_HINT=auto
+      export LIBVA_DRIVER_NAME=nvidia
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
     '';
   };
 }
