fd455fc5426aa41216f828d8c701a8599cefd140
willg <greenlee04@gmail.com>
2024-12-31 11:26:11 -0500

    tweaks

--- modules/nixos/optional/regreet.nix
+++ modules/nixos/optional/regreet.nix
@@ -2,6 +2,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   moduleName = "regreet";
@@ -12,7 +13,10 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.greetd.enable = true; # greeter service
+    services.greetd = {
+      enable = true; # greeter service
+      settings.default_session.command = "${lib.getExe pkgs.cage} -s -m last -- ${lib.getExe config.programs.regreet.package}";
+    };
     programs.regreet.enable = true; # greeter
   };
 }
