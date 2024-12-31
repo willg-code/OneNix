daec213bd322ef3463238e2a721c87c1631cb325
willg <greenlee04@gmail.com>
2024-12-31 01:13:45 -0500

    remove enumerated dep

--- modules/home-manager/optional/gui/zen-browser.nix
+++ modules/home-manager/optional/gui/zen-browser.nix
@@ -16,7 +16,6 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = [
       inputs.zen-browser.packages.${pkgs.system}.default
-      pkgs.speechd # for speech synthesis
     ];
 
     # add as default app
