131a14645c1ecfca2a8f7954cc0c2881757b95c6
willg <greenlee04@gmail.com>
2024-12-31 11:00:40 -0500

    tweaks

--- machines/andromeda/default.nix
+++ machines/andromeda/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware.nix
     ./disko.nix
@@ -7,20 +7,21 @@
   # Modules
   modules.nixos.bluetooth.enable = true;
   modules.nixos.gamemode.enable = true;
-  modules.nixos.regreet.enable = true;
   modules.nixos.hyprland.enable = true;
   modules.nixos.networkmanager.enable = true;
   modules.nixos.nvidia.enable = true;
   modules.nixos.optimize-store.enable = true;
   modules.nixos.pipewire.enable = true;
   modules.nixos.printing.enable = true;
+  modules.nixos.regreet.enable = true;
   modules.nixos.steam.enable = true;
   modules.nixos.systemd-boot.enable = true;
 
   stylix = {
     enable = true;
-    image = ./assets/theme-image.jpg;
+    image = ./assets/andromeda.jpg;
     polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-sulphurpool.yaml";
   };
 
   # Other
--- modules/nixos/optional/regreet.nix
+++ modules/nixos/optional/regreet.nix
@@ -12,7 +12,10 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.greetd.enable = true; # greeter service
+    services.greetd = {
+      enable = true; # greeter service
+      settings.default_session.command = "cage -s -m last -- regreet";
+    };
     programs.regreet.enable = true; # greeter
   };
 }
