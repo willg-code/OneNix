3364c62cabe9bd96d25152b325895fc175913680
willg <greenlee04@gmail.com>
2025-01-04 22:32:52 -0500

    fix monitors after swap

--- modules/home-manager/optional/gui/hyprland/monitors.nix
+++ modules/home-manager/optional/gui/hyprland/monitors.nix
@@ -9,7 +9,7 @@
     ]
     ++ lib.optionals (hostName == "andromeda") [
       "HDMI-A-2, 1920x1080@144, 1600x-1080, 1, vrr, 0"
-      "DP-5, 2560x1440@180, 0x0, 1, bitdepth, 10, vrr, 1"
-      "DP-6, 2560x1440@180, 2560x0, 1, bitdepth, 10, vrr, 1"
+      "DP-4, 2560x1440@180, 0x0, 1, bitdepth, 10, vrr, 1"
+      "DP-5, 2560x1440@180, 2560x0, 1, bitdepth, 10, vrr, 1"
     ];
 }
