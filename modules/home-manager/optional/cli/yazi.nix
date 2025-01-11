e986127642f6ae8e667fab30f1fd7e4a131669af
willg <greenlee04@gmail.com>
2025-01-11 12:48:19 -0500

    zsh integration

--- modules/home-manager/optional/cli/yazi.nix
+++ modules/home-manager/optional/cli/yazi.nix
@@ -16,5 +16,17 @@ in {
     home.packages = [
       pkgs.yazi
     ];
+
+    # Zsh integration
+    programs.zsh.initExtra = ''
+      function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+          builtin cd -- "$cwd"
+        fi
+        rm -f -- "$tmp"
+      }
+    '';
   };
 }
