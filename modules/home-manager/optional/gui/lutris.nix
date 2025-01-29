537893e93adee412044907255535d70c1477fa9c
willg <greenlee04@gmail.com>
2025-01-28 19:22:55 -0500

    wine unstable

--- modules/home-manager/optional/gui/lutris.nix
+++ modules/home-manager/optional/gui/lutris.nix
@@ -15,7 +15,7 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = [
       pkgs.lutris
-      pkgs.wineWowPackages.waylandFull
+      pkgs.wineWowPackages.unstableFull
       pkgs.gamescope
     ];
   };
