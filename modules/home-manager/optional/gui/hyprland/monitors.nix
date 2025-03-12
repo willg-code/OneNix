84d8738adde3c24e1b9ed3fc30b1d4388515e3fa
willg <greenlee04@gmail.com>
2025-03-12 16:04:17 -0400

    no vrr in monitor config

--- modules/home-manager/optional/gui/hyprland/monitors.nix
+++ modules/home-manager/optional/gui/hyprland/monitors.nix
@@ -8,8 +8,8 @@
       "FALLBACK,1920x1080@60,auto,1" # fallback monitor config that generally works
     ]
     ++ lib.optionals (hostName == "andromeda") [
-      "DP-4, 2560x1440@180, 0x0, 1, bitdepth, 10, vrr, 1"
-      "DP-5, 2560x1440@180, 2560x0, 1, bitdepth, 10, vrr, 1"
-      "HDMI-A-2, 1920x1080@144, 1600x-1080, 1, vrr, 0"
+      "DP-4, 2560x1440@180, 0x0, 1, bitdepth, 10"
+      "DP-5, 2560x1440@180, 2560x0, 1, bitdepth, 10"
+      "HDMI-A-2, 1920x1080@144, 1600x-1080, 1"
     ];
 }
